050c9c7f6e063f6c41c8b2d2f3a81d47eaa4a8e6
Agung Sundoro <muhagung2001@gmail.com>
2025-02-05 20:03:22 +0700

    feat: learn data binding and methods

---
+++ app.js
@@ -0,0 +1,22 @@
+const { createApp, ref } = Vue
+
+// Create and mount the app
+const app = createApp({
+    // Data binding
+    data() {
+        return {
+            title: 'Learn Vue JS',
+            author: 'Agung',
+            location: 'Jakarta',
+            age: 20
+        }
+    },
+
+    // Methods
+    methods: {
+        changeAuthor() {
+            this.author = 'Sundoro'
+        }
+    }
+})
+app.mount('#app')
