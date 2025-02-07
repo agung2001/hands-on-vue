add35257c8ced907904af9067e2e604efd7a2a4e
Agung Sundoro <muhagung2001@gmail.com>
2025-02-07 13:51:40 +0700

    feat: implement compute method for filtering

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const { createApp, ref } = Vue
 
 // Create and mount the app
 const app = createApp({
-    // Data binding
+    // Data binding : data that can be used in the template
     data() {
         return {
             title: 'Learn Vue JS',
@@ -18,21 +18,26 @@ const app = createApp({
             portfolio: [
                 {
                     name: 'Portfolio 1',
-                    url: 'https://portfolio1.com'
+                    url: 'https://portfolio1.com',
+                    isVue: true
                 },
                 {
                     name: 'Portfolio 2',
-                    url: 'https://portfolio2.com'
+                    url: 'https://portfolio2.com',
+                    isVue: true
                 },
                 {
                     name: 'Portfolio 3',
-                    url: 'https://portfolio3.com'
+                    url: 'https://portfolio3.com',
+                    isVue: true
                 }
             ]
         }
     },
 
-    // Methods
+    // Methods : function that can be used in the template
+    // Re-run every time they are called. Use for logic that needs to execute on demand (e.g., event handlers).
+    // https://www.w3schools.com/vue/vue_methods.php
     methods: {
         changeAuthor() {
             this.author = 'Sundoro'
@@ -43,6 +48,19 @@ const app = createApp({
         handleMouseMoveEvent(event) {
             this.mouse.x = event.clientX
             this.mouse.y = event.clientY
+        },
+        toggleVuePortfolio(item) {
+            console.log(item)
+            item.isVue = !item.isVue
+        }
+    },
+
+    // Computed : a property used to define reactive derived state. It allows you to create a value that is automatically updated when its dependencies change. 
+    // Computed properties are cached based on their dependencies, making them efficient for performing calculations or transformations on data.
+    // Cached based on dependencies. Use for derived data that doesn't change often.
+    computed: {
+        filteredPortfolio() {
+            return this.portfolio.filter(item => item.isVue)
         }
     }
 })
