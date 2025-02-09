4cc7008bd2cadd6edb1ef8397d50ca3ae73d4908
Agung Sundoro <muhagung2001@gmail.com>
2025-02-09 15:51:19 +0700

    feat: implement lifecycle hook mounted

--- modal-project/src/components/Main.vue
+++ modal-project/src/components/Main.vue
@@ -1,23 +1,6 @@
 <script setup lang="ts">
-import { reactive } from 'vue';
-import Modal from './Modal.vue'
 import Reference from './Reference.vue';
-import { IModal } from '../types'
-
-const modals: IModal[] = reactive([
-    {
-        title: 'Modal',
-        description: 'This is a modal',
-        image: 'https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExeXp6MmxnNnhnYml5ZXF0eWJkcWw1NGlma2VkNHRmOWdsY203MndpdiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/tpVKvAabWt3G5csMkT/giphy.gif',
-        isOpen: false
-    },
-    {
-        title: 'Modal 2',
-        description: 'This is a modal 2',
-        image: 'https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExa2RpbWtwMTFsYzBycXNsbmdnZDE5MzZpcmUxc3JvOHhkNWhyN2FjaSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/iVddRfGnwPd4TnZFpo/giphy.gif',
-        isOpen: false
-    }
-])
+import Modals from './Modals.vue';
 </script>
 
 <template>
@@ -28,29 +11,7 @@ const modals: IModal[] = reactive([
         <Reference />
 
         <h2>Modal</h2>
-        <template v-for="(modal, index) in modals" :key="index">  
-            <teleport to=".modals">
-                <Modal 
-                    :modal="modal" 
-                    @open="() => { modal.isOpen = true }"
-                    @close="() => { modal.isOpen = false }" 
-                >
-                    <template #content>
-                        <img
-                            width="100px" 
-                            :src="modal.image"
-                            :alt="modal.title"
-                        >
-                    </template>
-                    <template #footer>
-                        Copyright {{ new Date().getFullYear() }} by Agung Sundoro
-                    </template>
-                </Modal>
-            </teleport>  
-            <button @click="() => { modals[index].isOpen = true }">
-                Open {{ modal.title }}
-            </button>
-        </template>
+        <Modals />
     </div>
 </template>
 
--- modal-project/src/components/Modal.vue
+++ modal-project/src/components/Modal.vue
@@ -1,20 +1,27 @@
 <script setup lang="ts">
+import { onMounted, onUnmounted } from 'vue';
 import { IModal } from '../types'
 
 const { modal } = defineProps<{ modal: IModal }>()
 const emit = defineEmits(['open', 'close'])
+
+onMounted(() => {
+    console.log(`${modal.title} is mounted`)
+})
+
+onUnmounted(() => {
+    console.log(`${modal.title} is unmounted`)
+})
 </script>
 
 <template>
-    <div v-if="modal.isOpen">
-        <div class="backdrop" @click.self="() => { emit('close') }">
-            <div class="modal">
-                <h1>{{ modal.title }}</h1>
-                <p>{{ modal.description }}</p>
-                <slot name="content"></slot>
-                <hr>
-                <slot name="footer"></slot>
-            </div>
+    <div class="backdrop" @click.self="() => { emit('close') }">
+        <div class="modal">
+            <h1>{{ modal.title }}</h1>
+            <p>{{ modal.description }}</p>
+            <slot name="content"></slot>
+            <hr>
+            <slot name="footer"></slot>
         </div>
     </div>
 </template>
