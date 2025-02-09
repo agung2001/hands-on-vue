98b20ec086d69ee340fa684a30ed9216bf2dcf97
Agung Sundoro <muhagung2001@gmail.com>
2025-02-09 20:55:01 +0700

    fix: post page not showing detail page

--- modal-project/src/components/pages/Post.vue
+++ modal-project/src/components/pages/Post.vue
@@ -1,16 +1,26 @@
 <script setup>
-import { defineProps } from 'vue';
+import { defineProps, onMounted, ref } from 'vue';
 
+const post = ref({})
 const props = defineProps({
   id: String
+})
+
+// Load post from jsonplaceholder
+onMounted(async () => {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${props.id}`)
+    const data = await response.json()
+    post.value = data
+    console.log('✅ Post loaded:', data)
 })
 </script>
 
 <template>
     <h1 class="text-4xl font-bold py-4">
-        Portfolio number 
+        {{ post.title }}
         <span class="text-red-500">
-            {{ props.id }}
+            (#{{ props.id }})
         </span>
     </h1>
+    <p class="text-gray-500">{{ post.body }}</p>
 </template>
