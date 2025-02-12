be23cd53c9606bc0d51b404ba1a1c1d644255344
Agung Sundoro <muhagung2001@gmail.com>
2025-02-12 19:59:49 +0700

    feat: add task form and restructure task page

--- src/components/pages/Tasks.vue
+++ src/components/pages/Tasks.vue
@@ -1,39 +1,75 @@
 <script setup>
-import { useTaskStore } from '../../stores/TaskStore';
-
+import { useTaskStore } from "../../stores/TaskStore";
+import { ref } from "vue";
 const taskStore = useTaskStore();
+const newTask = ref("");
 </script>
 
 <template>
   <h1 class="text-4xl font-bold py-4">Tasks</h1>
 
-  <div class="flex gap-2 pb-4">
-    <p class="bg-gray-100 px-4 py-2 rounded-full text-sm font-bold">Total tasks: {{ taskStore.totalTasks }}</p>
-    <p class="bg-gray-100 px-4 py-2 rounded-full text-sm font-bold">Total completed tasks: {{ taskStore.totalCompletedTasks }}</p>
-    <p class="bg-gray-100 px-4 py-2 rounded-full text-sm font-bold">Total incomplete tasks: {{ taskStore.totalIncompleteTasks }}</p>
+  <div class="flex gap-2 py-2 justify-between">
+    <form @submit.prevent="taskStore.addTask(newTask)" class="flex gap-2">
+      <input
+        v-model="newTask"
+        type="text"
+        placeholder="Enter task title"
+        class="border border-gray-300 px-4 py-2 rounded"
+      />
+      <button type="submit" class="cursor-pointer bg-blue-500 text-white px-4 py-2 rounded">
+        Add Task
+      </button>
+    </form>
   </div>
-  <div class="flex gap-2 pb-4 justify-between">
-    <button @click="taskStore.addTask('Task 1')" class="bg-blue-500 text-white px-4 py-2">Add Task</button>
+
+  <div class="flex gap-2 py-2 justify-between">
+    <div class="flex gap-2">
+      <p class="bg-gray-100 px-4 py-2 rounded-full text-sm font-bold">
+        Total tasks: {{ taskStore.totalTasks }}
+      </p>
+      <p class="bg-gray-100 px-4 py-2 rounded-full text-sm font-bold">
+        Total completed tasks: {{ taskStore.totalCompletedTasks }}
+      </p>
+      <p class="bg-gray-100 px-4 py-2 rounded-full text-sm font-bold">
+        Total incomplete tasks: {{ taskStore.totalIncompleteTasks }}
+      </p>
+    </div>
 
     <div class="flex gap-2">
-      <button @click="taskStore.filter = 'all'" class="cursor-pointer bg-gray-100 px-4 py-2 rounded-full text-sm font-bold">All</button>
-      <button @click="taskStore.filter = 'completed'" class="cursor-pointer bg-gray-100 px-4 py-2 rounded-full text-sm font-bold">Completed</button>
-      <button @click="taskStore.filter = 'incomplete'" class="cursor-pointer bg-gray-100 px-4 py-2 rounded-full text-sm font-bold">Incomplete</button>
+      <button
+        @click="taskStore.filter = 'all'"
+        class="cursor-pointer bg-gray-100 px-4 py-2 rounded-full text-sm font-bold"
+      >
+        All
+      </button>
+      <button
+        @click="taskStore.filter = 'completed'"
+        class="cursor-pointer bg-gray-100 px-4 py-2 rounded-full text-sm font-bold"
+      >
+        Completed
+      </button>
+      <button
+        @click="taskStore.filter = 'incomplete'"
+        class="cursor-pointer bg-gray-100 px-4 py-2 rounded-full text-sm font-bold"
+      >
+        Incomplete
+      </button>
     </div>
   </div>
 
-  <div class="flex flex-col gap-4 pt-4">
-    <div 
-      v-for="task in taskStore.filteredTasks" :key="task.id" 
-      class="cursor-pointer bg-gray-100 p-4 rounded-md flex justify-between" 
+  <div class="flex flex-col gap-4">
+    <div
+      v-for="task in taskStore.filteredTasks"
+      :key="task.id"
+      class="cursor-pointer bg-gray-100 p-4 rounded-md flex justify-between"
       :class="{ 'bg-green-500': task.completed, 'bg-yellow-500': !task.completed }"
       @click="taskStore.toggleTask(task.id)"
     >
       <h2>{{ task.title }}</h2>
 
       <button @click="taskStore.deleteTask(task.id)" class="bg-red-500 text-white px-4 py-2">
-          <i class="fa-solid fa-trash"></i>
-        </button>
+        <i class="fa-solid fa-trash"></i>
+      </button>
     </div>
   </div>
 </template>
--- src/stores/TaskStore.js
+++ src/stores/TaskStore.js
@@ -1,55 +1,57 @@
 import { defineStore } from 'pinia';
 
 export const useTaskStore = defineStore('taskStore', {
-    state: () => ({
-        tasks: [
-            {
-                id: 1,
-                title: 'Task 1',
-                completed: false,
-            },
-            {
-                id: 2,
-                title: 'Task 2',
-                completed: false,
-            },
-            {
-                id: 3,
-                title: 'Task 3',
-                completed: true,
-            },
-        ],
-        filter: 'all',
-    }),
-    getters: {
-        completedTasks: (state) => {
-            return state.tasks.filter((task) => task.completed);
-        },
-        totalTasks: (state) => {
-            return state.tasks.length;
-        },
-        totalCompletedTasks: (state) => {
-            return state.tasks.filter((task) => task.completed).length;
-        },
-        totalIncompleteTasks: (state) => {
-            return state.tasks.filter((task) => !task.completed).length;
-        },
-        filteredTasks: (state) => {
-            if (state.filter === 'all') {
-                return state.tasks;
-            }
-            return state.tasks.filter((task) => task.completed === (state.filter === 'completed'));
-        },
+  state: () => ({
+    tasks: [
+      {
+        id: 1,
+        title: 'Task 1',
+        completed: false,
+      },
+      {
+        id: 2,
+        title: 'Task 2',
+        completed: false,
+      },
+      {
+        id: 3,
+        title: 'Task 3',
+        completed: true,
+      },
+    ],
+    filter: 'all',
+  }),
+  getters: {
+    completedTasks: (state) => {
+      return state.tasks.filter((task) => task.completed);
     },
-    actions: {
-        addTask(task) {
-            this.tasks.push(task);
-        },
-        deleteTask(id) {
-            this.tasks = this.tasks.filter((task) => task.id !== id);
-        },
-        toggleTask(id) {
-            this.tasks = this.tasks.map((task) => task.id === id ? { ...task, completed: !task.completed } : task);
-        },
+    totalTasks: (state) => {
+      return state.tasks.length;
     },
+    totalCompletedTasks: (state) => {
+      return state.tasks.filter((task) => task.completed).length;
+    },
+    totalIncompleteTasks: (state) => {
+      return state.tasks.filter((task) => !task.completed).length;
+    },
+    filteredTasks: (state) => {
+      if (state.filter === 'all') {
+        return state.tasks;
+      }
+      return state.tasks.filter((task) => task.completed === (state.filter === 'completed'));
+    },
+  },
+  actions: {
+    addTask(title) {
+      this.tasks.push({ id: this.tasks.length + 1, title, completed: false });
+    },
+    deleteTask(id) {
+      this.tasks = this.tasks.filter((task) => task.id !== id);
+    },
+    toggleTask(id) {
+      this.tasks = this.tasks.map((task) =>
+        task.id === id ? { ...task, completed: !task.completed } : task
+      );
+    },
+  },
 });
